ff90b3a603cca279facf401080254b206e77f6f6
jrasero <jrasero.daparte@gmail.com>
2021-06-24 19:52:32 -0400

    correct scripts

--- scripts/ratings.sh
+++ scripts/ratings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python run_experiment_main.py --data_dir ../data --output_dir ../results/prediction_new --con_type look_neg_look_neut --target_var chg_LNeg_LNur > ratings_con_0001.out
-python run_experiment_main.py --data_dir ../data --output_dir ../results/prediction_new --con_type reg_neg_look_neg --target_var chg_RNeg_LNeg > ratings_con_0002.out
+python run_experiment_main.py --data_dir ../data --output_dir ../results/prediction --con_type look_neg_look_neut --target_var chg_LNeg_LNur > ratings_con_0001.out
+python run_experiment_main.py --data_dir ../data --output_dir ../results/prediction --con_type reg_neg_look_neg --target_var chg_RNeg_LNeg > ratings_con_0002.out
--- scripts/ratings_sensitivity.sh
+++ scripts/ratings_sensitivity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python run_experiment_sensitivity.py --data_dir ../data --output_dir ../results/prediction_sensitivity_new --con_type look_neg_look_neut --target_var chg_LNeg_LNur > ratings_sensitivity_con_0001.out
-python run_experiment_sensitivity.py --data_dir ../data --output_dir ../results/prediction_sensitivity_new --con_type reg_neg_look_neg --target_var chg_RNeg_LNeg > ratings_sensitivity_con_0002.out
+python run_experiment_sensitivity.py --data_dir ../data --output_dir ../results/prediction_sensitivity --con_type look_neg_look_neut --target_var chg_LNeg_LNur > ratings_sensitivity_con_0001.out
+python run_experiment_sensitivity.py --data_dir ../data --output_dir ../results/prediction_sensitivity --con_type reg_neg_look_neg --target_var chg_RNeg_LNeg > ratings_sensitivity_con_0002.out
